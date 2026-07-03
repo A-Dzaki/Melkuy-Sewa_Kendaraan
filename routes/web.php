8e8cb34c5ee1566a58586ee4d5b1f077bd7eb00b
A-Dzaki <dzakiahmad005@gmail.com>
2026-07-03 21:05:40 +0700

    penguabahn struktur file

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('User.dasboarduser');
-})->name('home');
+Route::get('/', fn() => view('layouts.User'))->name('home');
 
-Route::get('/daftar-kendaraan', function () {
-    return view('User.daftarkendaraan');
-})->name('user.daftar');
+Route::get('/daftar-kendaraan', fn() => view('user.daftarkendaraan'))->name('user.daftar');
 
-Route::get('/pencarian-kendaraan', function () {
-    return view('User.Pencariankendaraan');
-})->name('user.pencarian');
+Route::get('/pencarian-kendaraan', fn() => view('user.pencariankendaraan'))->name('user.pencarian');
 
-Route::get('/history', function () {
-    return view('User.historyusers');
-})->name('user.history');
+Route::get('/history', fn() => view('user.historyusers'))->name('user.history');
